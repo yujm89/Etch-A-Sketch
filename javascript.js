b186350e8f7af5a297f30b85333b7b1ca3691eed
yumsteroni <jgflores1029@gmail.com>
2025-03-29 04:50:46 -0400

    Add Reset button

--- javascript.js
+++ javascript.js
@@ -1,17 +1,28 @@
 const container = document.querySelector("#container");
+const resetButton = document.querySelector("#reset");
+
+resetButton.addEventListener("click", () => reset());
 
 
 for (let i = 0; i < 9801; i++){
     const gridItem = document.createElement("div");
+    gridItem.classList.add("gridItem")
     let gridItemSize = 500/99;
     gridItem.style.height = `${gridItemSize}px`;
     gridItem.style.width = `${gridItemSize}px`;
-    console.log(gridItemSize)
-
     container.appendChild(gridItem);
     gridItem.addEventListener("mouseover", function (e) {
         e.target.style.background = "black";
     })
 }
+
+
+function reset(){
+    const canvas = document.querySelectorAll(".gridItem");
+    canvas.forEach((item) => {
+        item.style.backgroundColor = "white";
+    })
+}
+
 
 
